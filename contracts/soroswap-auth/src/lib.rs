270ba480394f3cb795c878235f418378be5ce86b
coderipper <hello@joaquinsoza.dev>
2025-11-16 15:09:24 -0300

    working simple contract

--- contracts/soroswap-auth/src/lib.rs
+++ contracts/soroswap-auth/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 use soroban_sdk::{
-    contract, contractimpl, Address, Env, Vec,
+    auth::{ContractContext, InvokerContractAuthEntry, SubContractInvocation},
+    contract, contractimpl, vec, Address, Env, IntoVal, Symbol, Val, Vec,
 };
 
 mod soroswap_router;
@@ -22,10 +23,10 @@ pub fn check_nonnegative_amount(amount: i128) -> Result<(), SoroswapError> {
 }
 
 #[contract]
-struct SoroswapSimple;
+struct SoroswapAuth;
 
 #[contractimpl]
-impl SoroswapSimple {
+impl SoroswapAuth {
     pub fn __constructor(e: Env, router_address: Address) {
         set_soroswap_router_address(&e, router_address);
     }
@@ -39,9 +40,28 @@ impl SoroswapSimple {
         let soroswap_router_address = get_soroswap_router_address(&e);
         let soroswap_router_client = SoroswapRouterClient::new(&e, &soroswap_router_address);
 
+        let pair_address = soroswap_router_client.router_pair_for(&token_in, &token_out);
+
         let mut path: Vec<Address> = Vec::new(&e);
         path.push_back(token_in.clone());
         path.push_back(token_out.clone());
+
+        let mut swap_args: Vec<Val> = vec![&e];
+        swap_args.push_back(caller.into_val(&e)); // From
+        swap_args.push_back(pair_address.into_val(&e)); // To
+        swap_args.push_back(amount.into_val(&e)); // Amount
+
+        e.authorize_as_current_contract(vec![
+            &e,
+            InvokerContractAuthEntry::Contract(SubContractInvocation {
+                context: ContractContext {
+                    contract: token_in.clone(),
+                    fn_name: Symbol::new(&e, "transfer"),
+                    args: swap_args.clone(),
+                },
+                sub_invocations: vec![&e],
+            }),
+        ]);
 
         let swap_result = soroswap_router_client.swap_exact_tokens_for_tokens(
             &amount, // Amount In
@@ -52,6 +72,18 @@ impl SoroswapSimple {
         );
 
         let total_swapped_amount = swap_result.last().unwrap();
+
+        // // Add liquidity
+        // let _result = soroswap_router_client.add_liquidity(
+        //     &usdc_address,
+        //     &xlm_address,
+        //     &swap_amount,
+        //     &total_swapped_amount,
+        //     &0,
+        //     &0,
+        //     &from,
+        //     &u64::MAX,
+        // );
 
         Ok(total_swapped_amount)
     }
